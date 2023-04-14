6b71d2996d2f0fd1fd6bb7de216688c380f31e5e
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-14 11:14:28 +0500

    setup tableViewCell

--- IslamGuide.uz/ViewControllers/PrayingTimeViewController.swift
+++ IslamGuide.uz/ViewControllers/PrayingTimeViewController.swift
@@ -12,6 +12,7 @@ final class PrayingTimeViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     private let networkManager = NetworkManager.shared
+    private var prayingTime: Praying?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +26,12 @@ final class PrayingTimeViewController: UIViewController {
 private extension PrayingTimeViewController {
     func fetchData() {
         let url = URL(string: "https://dailyprayer.abdulrcs.repl.co/api/namangan")!
-        networkManager.fetch(Praying.self, from: url) { result in
+        networkManager.fetch(Praying.self, from: url) { [weak self] result in
             switch result {
                 case .success(let data):
                     print(data)
+                    self?.prayingTime = data
+                    self?.tableView.reloadData()
                 case .failure(let error):
                     print(error)
             }
@@ -39,13 +42,30 @@ private extension PrayingTimeViewController {
 //MARK: - UITableViewDataSource
 extension PrayingTimeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        6
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        content.text = "Praying Time"
+        
+        guard let prayingTime else { return cell}
+        
+        switch indexPath.row {
+            case 0:
+                content.text = "Bomdod: " + prayingTime.today.fajr
+            case 1:
+                content.text = "Quyosh chiqishi: " + prayingTime.today.sunrise
+            case 2:
+                content.text = "Peshin: " + prayingTime.today.dhuhr
+            case 3:
+                content.text = "Asr: " + prayingTime.today.asr
+            case 4:
+                content.text = "Shom: " + prayingTime.today.maghrib
+            default:
+                content.text = "Xufton: " + prayingTime.today.isha
+        }
+        
         cell.contentConfiguration = content
         return cell
     }
