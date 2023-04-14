88314d269474af1a08cb960643d95235374662ae
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-14 15:46:25 +0500

    work with ui

--- IslamGuide.uz/ViewControllers/TasbihViewController.swift
+++ IslamGuide.uz/ViewControllers/TasbihViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 final class TasbihViewController: UIViewController {
-    
-  
     @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var countLabel: UILabel!
+    
+    private var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func countButtonPressed() {
+        if count < 33 {
+            count += 1
+        }
+        countLabel.text = count.formatted()
+    }
 }
