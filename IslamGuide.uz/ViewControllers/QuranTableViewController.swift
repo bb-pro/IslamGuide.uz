f914d8ec3563299be40343f7f5dbf5ec0a0be24d
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-05-14 13:42:22 +0500

    bug fixing

--- IslamGuide.uz/ViewControllers/QuranTableViewController.swift
+++ IslamGuide.uz/ViewControllers/QuranTableViewController.swift
@@ -7,22 +7,25 @@
 
 import UIKit
 
-final class QuranTableViewController: UITableViewController {
+final class QuranTableViewController: UIViewController {
     var response: QuranResponse?
     private var arabicResponse: QuranResponse?
     private var quranVersion: String!
     private let networkManager = NetworkManager.shared
+    @IBOutlet var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         fetchArabic()
         fetchTranslation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         guard let preferredLanguage = Locale.preferredLanguages.first else { return }
-       
+        
         print(preferredLanguage)
     }
     
@@ -59,48 +62,34 @@ private extension QuranTableViewController {
             }
         }
     }
-  
+    
     
 }
 
 
 // MARK: - Table view data source
-extension QuranTableViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
+extension QuranTableViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        if section == 0 {
-            return 1
-        } else {
             return response?.data.surahs.count ?? 1
-        }
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 1 {
-            return "Surahs"
-        } else {
-            return nil
-        }
-    }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         40
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return 176
-        } else {
-            return 72
-        }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        72
     }
     
-    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         if let headerView = view as? UITableViewHeaderFooterView {
             if section == 0 {
                 headerView.backgroundView?.backgroundColor = UIColor(named: "background")
@@ -108,29 +97,26 @@ extension QuranTableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "titleCell", for: indexPath)
-            return cell
-        } else {
-            guard let surah = response?.data.surahs[indexPath.row] else {
-                      // Return an empty cell if response is nil
-                      return UITableViewCell()
-                  }
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! QuranCell
-            cell.titleEn.text = surah.englishName
-            cell.titleArabic.text = surah.name
-            cell.number.text = String(surah.number)
-            cell.numberOfVerses.text = String("Number of ayahs: \(surah.ayahs.count)")
-            return cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let surah = response?.data.surahs[indexPath.row] else {
+            // Return an empty cell if response is nil
+            return UITableViewCell()
         }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! QuranCell
+        cell.titleEn.text = surah.englishName
+        cell.titleArabic.text = surah.name
+        cell.number.text = String(surah.number)
+        cell.numberOfVerses.text = String("Number of ayahs: \(surah.ayahs.count)")
+        return cell
+        
     }
 }
 
 //MARK: - UITableViewDelegate
-extension QuranTableViewController {
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+extension QuranTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
