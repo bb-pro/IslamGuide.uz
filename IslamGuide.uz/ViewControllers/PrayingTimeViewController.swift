aaac6327fd125ef78de6c74e0501a3ad3f9fd5e0
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-05-16 18:29:35 +0500

    add praying times

--- IslamGuide.uz/ViewControllers/PrayingTimeViewController.swift
+++ IslamGuide.uz/ViewControllers/PrayingTimeViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import Adhan
 
 final class PrayingTimeViewController: UIViewController {
-
+    
     @IBOutlet var tableView: UITableView!
     
     private let networkManager = NetworkManager.shared
-    private var prayingTime: Praying?
+    private var prayingTime: PrayerTimes?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,16 +27,25 @@ final class PrayingTimeViewController: UIViewController {
 //MARK: - Networking
 private extension PrayingTimeViewController {
     func fetchData() {
-        let url = URL(string: "https://dailyprayer.abdulrcs.repl.co/api/namangan")!
-        networkManager.fetch(Praying.self, from: url) { [weak self] result in
-            switch result {
-                case .success(let data):
-                    self?.prayingTime = data
-                    self?.tableView.reloadData()
-                case .failure(let error):
-                    print(error)
-            }
-        }
+        let cal = Calendar(identifier: Calendar.Identifier.gregorian)
+        let date = cal.dateComponents([.year, .month, .day], from: Date())
+        let coordinates = Coordinates(latitude: 40, longitude: 71)
+        var params = CalculationMethod.moonsightingCommittee.params
+        params.madhab = .hanafi
+        guard let prayers = PrayerTimes(coordinates: coordinates, date: date, calculationParameters: params) else { return }
+
+     
+//        print("fajr \(formatter.string(from: prayers.fajr))")
+//        print("sunrise \(formatter.string(from: prayers.sunrise))")
+//        print("dhuhr \(formatter.string(from: prayers.dhuhr))")
+//        print("asr \(formatter.string(from: prayers.asr))")
+//        print("maghrib \(formatter.string(from: prayers.maghrib))")
+//        print("isha \(formatter.string(from: prayers.isha))")
+        
+        prayingTime = prayers
+        
+        
+        
     }
 }
 
@@ -44,33 +54,39 @@ extension PrayingTimeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         6
     }
+  
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath) as! PrayingTimeCell
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm" // Hours and minutes format
+        
         guard let prayingTime else { return cell }
         
         switch indexPath.row {
             case 0:
-                cell.nameLabel.text = "Bomdod: "
-                cell.timeLabel.text = prayingTime.today.fajr
+                cell.nameLabel.text = "Fajr"
+                cell.timeLabel.text = formatter.string(from: prayingTime.fajr)
             case 1:
-                cell.nameLabel.text = "Quyosh chiqishi: "
-                cell.timeLabel.text = prayingTime.today.sunrise
+                cell.nameLabel.text = "Sunrise"
+                cell.timeLabel.text = formatter.string(from: prayingTime.sunrise)
             case 2:
-                cell.nameLabel.text = "Peshin: "
-                cell.timeLabel.text = prayingTime.today.dhuhr
+                cell.nameLabel.text = "Dhuhr"
+                cell.timeLabel.text = formatter.string(from: prayingTime.dhuhr)
             case 3:
-                cell.nameLabel.text = "Asr: "
-                cell.timeLabel.text = prayingTime.today.asr
+                cell.nameLabel.text = "Asr"
+                cell.timeLabel.text = formatter.string(from: prayingTime.asr)
             case 4:
-                cell.nameLabel.text = "Shom: "
-                cell.timeLabel.text = prayingTime.today.maghrib
+                cell.nameLabel.text = "Maghrib"
+                cell.timeLabel.text = formatter.string(from: prayingTime.maghrib)
             default:
-                cell.nameLabel.text = "Xufton: "
-                cell.timeLabel.text = prayingTime.today.isha
+                cell.nameLabel.text = "Isha"
+                cell.timeLabel.text = formatter.string(from: prayingTime.isha)
         }
         return cell
     }
+
 }
 //MARK: - UITableViewDelegate
 extension PrayingTimeViewController: UITableViewDelegate {
