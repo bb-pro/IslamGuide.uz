feea73dc066dcb17f45745ba1dc0604b28779c50
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-05-23 20:58:41 +0500

    work with settings

--- IslamGuide.uz/ViewControllers/SettingsViewController.swift
+++ IslamGuide.uz/ViewControllers/SettingsViewController.swift
@@ -46,6 +46,72 @@ extension SettingsViewController: UITableViewDataSource {
 //MARK: - UITableViewDelegate
 extension SettingsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.row {
+            case 0:
+                tableView.deselectRow(at: indexPath, animated: true)
+            case 1:
+                shareApp()
+            case 2:
+                performSegue(withIdentifier: "showInfo", sender: nil)
+            case 3:
+                openLink(urlString: "https://www.linkedin.com/in/bektemur-mamashaev-674a42216/")
+            default:
+                tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+}
+
+//MARK: - Private Methods
+private extension SettingsViewController {
+    func openLink(urlString: String) {
+        if let url = URL(string: urlString) {
+            // Open the URL
+            UIApplication.shared.open(url, options: [:]) { success in
+                if success {
+                    // The URL was opened successfully
+                    print("URL opened successfully")
+                } else {
+                    // Failed to open the URL
+                    print("Failed to open URL")
+                }
+            }
+        } else {
+            // Invalid URL
+            print("Invalid URL")
+        }
+    }
+
+    
+    func shareApp() {
+            // Create an instance of the UIActivityViewController
+            let activityViewController = UIActivityViewController(activityItems: [AppShareItem()], applicationActivities: nil)
+            
+            // Exclude some activity types if desired
+            activityViewController.excludedActivityTypes = [.airDrop, .addToReadingList]
+            
+            // Present the UIActivityViewController
+            present(activityViewController, animated: true, completion: nil)
+        }
+}
+
+// Custom UIActivityItemSource class to provide the app's information
+class AppShareItem: NSObject, UIActivityItemSource {
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return ""
+    }
+    
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        // Provide the content to be shared
+        return "Check out this awesome app: IslamGuide"
+    }
+    
+    func activityViewController(_ activityViewController: UIActivityViewController, subjectForActivityType activityType: UIActivity.ActivityType?) -> String {
+        // Provide a subject for the sharing activity
+        return "IslamGuide"
+    }
+    
+    func activityViewController(_ activityViewController: UIActivityViewController, dataTypeIdentifierForActivityType activityType: UIActivity.ActivityType?) -> String {
+        // Specify the type of data being shared (e.g., plain text, URL)
+        return "public.text"
     }
 }
